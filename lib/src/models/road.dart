2b0ef33d3323ffa487495d912dcc395c29d0e159
dali <lionelviperdali@gmail.com>
2021-10-30 16:15:21 +0100

    feat : parse road json * create compute method to parse road json to road object * create localisation to make instruction multi-lang

--- lib/src/models/road.dart
+++ lib/src/models/road.dart
@@ -2,28 +2,80 @@ import 'package:osrm_dart/src/models/lng_lat.dart';
 
 class Road {
   final double distance;
-  final double time;
-  List<RoadInstructions> instructions = [];
+  final double duration;
+  List<RoadInstruction> instructions = [];
+  final String polylineEncoded;
+  bool _isError = false;
+  RoadDetailInfo details = RoadDetailInfo();
 
-  Road.minimize({required this.distance, required this.time});
+  Road.empty()
+      : distance = 0.0,
+        duration = 0.0,
+        polylineEncoded = "";
+
+  Road.minimize({
+    required this.distance,
+    required this.duration,
+    required this.polylineEncoded,
+  });
 
   Road({
     required this.distance,
-    required this.time,
+    required this.duration,
     required this.instructions,
+    required this.polylineEncoded,
   });
 
   Road.withError()
-      : time = 0.0,
-        distance = 0.0;
+      : duration = 0.0,
+        distance = 0.0,
+        polylineEncoded = "",
+        _isError = true;
+
+  bool get canDrawRoad => !_isError;
+
+  Road copyWith({
+    double? distance,
+    double? duration,
+    List<RoadInstruction>? instructions,
+    String? polylineEncoded,
+  }) {
+    return Road(
+      distance: distance ?? this.distance,
+      duration: duration ?? this.duration,
+      instructions: instructions ?? this.instructions,
+      polylineEncoded: polylineEncoded ?? this.polylineEncoded,
+    );
+  }
 }
 
-class RoadInstructions {
+class RoadInstruction {
+  double distance;
+  double duration;
   final String instruction;
   final LngLat location;
 
-  RoadInstructions({
+  RoadInstruction({
+
+    required this.distance,
+    required this.duration,
     required this.instruction,
     required this.location,
   });
 }
+
+class RoadDetailInfo {
+  List<RoadLeg> roadLegs = [];
+  RoadDetailInfo();
+}
+
+class RoadLeg {
+  final double distance;
+  final double duration;
+
+  RoadLeg(
+    this.distance,
+    this.duration,
+  );
+}
+
--- lib/src/osrm_manager.dart
+++ lib/src/osrm_manager.dart
@@ -1,9 +1,69 @@
 import 'dart:convert';
 
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:osrm_dart/osrm_dart.dart';
 
 import 'models/lng_lat.dart';
+import 'models/road.dart';
+import 'utilities/localisation_instruction.dart';
 import 'utilities/utils.dart';
+
+Future<Road> parseRoad(List<dynamic> data) async {
+  Map<String, dynamic> jsonResponse = data.first;
+  String languageCode = data.last;
+  var road = Road.empty();
+  final Map<String, dynamic> routes = jsonResponse["routes"];
+
+  routes.forEach((key, route) {
+    final distance = (route["distance"] as double) / 1000;
+    final duration = route["duration"] as double;
+    final mRouteHigh = route["geometry"] as String;
+    road = road.copyWith(
+      distance: distance,
+      duration: duration,
+      polylineEncoded: mRouteHigh,
+    );
+    if ((route as Map).containsKey("legs")) {
+      final Map<String, dynamic> mapLegs = route["legs"];
+      mapLegs.forEach((key, leg) {
+        final RoadLeg legRoad = RoadLeg(
+          leg["distance"],
+          leg["duration"],
+        );
+        road.details.roadLegs.add(legRoad);
+        if ((leg as Map<String, dynamic>).containsKey("steps")) {
+          final Map<String, dynamic> steps = leg["steps"];
+          steps.forEach((key, step) {
+            final location = LngLat(
+              lat: step["location"][1],
+              lng: step["location"][0],
+            );
+            Map<String, dynamic> maneuver = step["maneuver"];
+            String direction = maneuver["type"];
+            String name = step["name"] ?? "";
+            String instruction = OSRMManager.instructionFromDirection(
+              direction,
+              maneuver,
+              name,
+              languageCode,
+            );
+            RoadInstruction roadInstruction = RoadInstruction(
+              distance: step["distance"],
+              duration: step["duration"],
+              instruction: instruction,
+              location: location,
+            );
+            road.instructions.add(roadInstruction);
+          });
+        }
+      });
+    }
+  });
+
+  return road;
+}
 
 class OSRMManager {
   final String server;
@@ -19,15 +79,16 @@ class OSRMManager {
     this.roadType = RoadType.car,
   });
 
-  Future getRoad({
+  Future<Road> getRoad({
     required List<LngLat> waypoints,
     RoadType roadType = RoadType.car,
     int alternative = 0,
     bool steps = true,
     Overview overview = Overview.full,
     Geometries geometrie = Geometries.geojson,
+    String languageCode = "en",
   }) async {
-    String path = await generatePath(
+    String path = generatePath(
       waypoints.toWaypoints(),
       alternative: alternative,
       steps: steps,
@@ -36,10 +97,39 @@ class OSRMManager {
     );
     final response = await dio.get(path);
     if (response.statusCode == 200) {
-      final responseJson = json.decode(response.data);
+      final Map<String, dynamic> responseJson = json.decode(response.data);
+      return compute(parseRoad, [responseJson, languageCode]);
     } else {
-      return null;
+      return Road.withError();
     }
+  }
+
+  static String instructionFromDirection(
+    String direction,
+    Map<String, dynamic> jManeuver,
+    String roadName,
+    String languageCode,
+  ) {
+    if (direction == "turn" || direction == "ramp" || direction == "merge") {
+      String modifier = jManeuver["modifier"];
+      direction = "$direction-$modifier";
+    } else if (direction == "roundabout") {
+      int exit = jManeuver["exit"];
+      direction = "$direction-$exit";
+    } else if (direction == "rotary") {
+      int exit = jManeuver["exit"];
+      direction = "roundabout-$exit"; //convert rotary in roundabout...
+    }
+    int maneuverCode = 0;
+    if (maneuvers.containsKey(direction)) {
+      maneuverCode = maneuvers[direction] ?? 0;
+    }
+    return LocalisationInstruction(
+      languageCode: languageCode,
+    ).getInstruction(
+      maneuverCode,
+      roadName,
+    );
   }
 }
 
@@ -52,7 +142,7 @@ extension OSRMPrivateFunct on OSRMManager {
     bool steps = true,
     Overview overview = Overview.full,
     Geometries geometrie = Geometries.polyline,
-  })  {
+  }) {
     String url = "$server/routed-${roadType.value}/route/v1/diving/$waypoints";
     String option = "";
     option += "alternatives=${alternative <= 0 ? false : alternative}&";
