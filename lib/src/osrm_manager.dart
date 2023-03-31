a38c147f92281907c228bb425aefd5b55e064b47
liodali <lionelviperdali@gmail.com>
2023-03-31 18:52:03 +0200

    feature : re-implement instruction fron osrm api * replace instructionFromDirection by buildInstruction

--- lib/src/osrm_manager.dart
+++ lib/src/osrm_manager.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
+import 'package:routing_client_dart/src/models/road_helper.dart';
 import 'utilities/computes_utilities.dart';
 
 import 'models/lng_lat.dart';
@@ -8,15 +9,15 @@ import 'utilities/localisation_instruction.dart';
 import 'utilities/utils.dart';
 
 /// [OSRMManager]
-/// 
+///
 /// this class responsible to manage http call to get road from open-source osm server
 /// or custom server that should be specified in constructor based on osrm project
 /// contain only one public method [getRoad] to make the call
 /// and return [Road] object.
-/// 
+///
 /// for more detail see : https://github.com/Project-OSRM/osrm-backend
-/// 
-/// 
+///
+///
 /// [server]   : (String) represent the osm server or any custom server that based of OSRM project
 ///
 /// [roadType] : (RoadType) represent the type of road that you want to use, car or foot,bike only for osm servers
@@ -35,11 +36,11 @@ class OSRMManager {
   });
 
   /// [getRoad]
-  /// 
+  ///
   /// this method make http call to get road from specific server
   /// this method return Road that contain road information like distance and duration
   /// and instruction or return Road with empty values.
-  /// 
+  ///
   /// return Road object that contain information of road
   /// that will help to draw road in the map or show important information to the user
   /// you should take a specific case when road object will contain empty values like 0.0 or empty string
@@ -80,8 +81,8 @@ class OSRMManager {
   /// [getTrip]
   /// this method used to get route from trip service api
   /// used if you have more that 10 waypoint to generate route will more accurate
-  /// than [getRoad]. 
-  /// 
+  /// than [getRoad].
+  ///
   /// Please note that if one sets [roundTrip] to false, then
   /// [source] and [destination] must be provided.
   Future<Road> getTrip({
@@ -125,32 +126,135 @@ class OSRMManager {
     }
   }
 
-  static String instructionFromDirection(
-    String direction,
-    Map<String, dynamic> jManeuver,
-    String roadName,
-    String languageCode,
+  static String buildInstruction(
+    RoadStep step,
+    Map<String, dynamic> instructionsHelper,
+    Map<String, dynamic> option,
   ) {
-    if (direction == "turn" || direction == "ramp" || direction == "merge") {
-      String modifier = jManeuver["modifier"];
-      direction = "$direction-$modifier";
-    } else if (direction == "roundabout") {
-      int exit = jManeuver["exit"];
-      direction = "$direction-$exit";
-    } else if (direction == "rotary") {
-      int exit = jManeuver["exit"];
-      direction = "roundabout-$exit"; //convert rotary in roundabout...
+    var type = step.maneuver.maneuverType;
+    final instructionsV5 = instructionsHelper['v5'] as Map<String, dynamic>;
+    if (!instructionsV5.containsKey(type)) {
+      type = 'turn';
     }
-    int maneuverCode = 0;
-    if (maneuvers.containsKey(direction)) {
-      maneuverCode = maneuvers[direction] ?? 0;
+
+    var instructionObject = (instructionsV5[type]
+        as Map<String, dynamic>)['default'] as Map<String, dynamic>;
+    final omitSide = type == 'off ramp' &&
+        ((step.maneuver.modifier?.indexOf(step.drivingSide) ?? 0) >= 0);
+    if (step.maneuver.modifier != null &&
+        (instructionsV5[type] as Map<String, dynamic>)
+            .containsKey(step.maneuver.modifier!) &&
+        !omitSide) {
+      instructionObject = (instructionsV5[type]
+              as Map<String, dynamic>)[step.maneuver.modifier!]
+          as Map<String, dynamic>;
     }
-    return LocalisationInstruction(
-      languageCode: languageCode,
-    ).getInstruction(
-      maneuverCode,
-      roadName,
-    );
+    String? laneInstruction;
+    switch (step.maneuver.maneuverType) {
+      case 'use lane':
+        final lane = laneConfig(step);
+        if (lane != null) {
+          laneInstruction = (((instructionsV5[type]
+                  as Map<String, dynamic>)['constants']
+              as Map<String, dynamic>)['lanes'] as Map<String, String>)[lane];
+        } else {
+          instructionObject = ((instructionsV5[type]
+                  as Map<String, dynamic>)[step.maneuver.maneuverType]
+              as Map<String, dynamic>)['no_lanes'] as Map<String, dynamic>;
+        }
+        break;
+      case 'rotary':
+      case 'roundabout':
+        if (step.rotaryName != null &&
+            step.maneuver.exit != null &&
+            instructionObject.containsKey('name_exit')) {
+          instructionObject =
+              instructionObject['name_exit'] as Map<String, dynamic>;
+        } else if (step.rotaryName != null &&
+            instructionObject.containsKey('name')) {
+          instructionObject = instructionObject['name'] as Map<String, dynamic>;
+        } else if (step.maneuver.exit != null &&
+            instructionObject.containsKey('exit')) {
+          instructionObject = instructionObject['exit'] as Map<String, dynamic>;
+        } else {
+          instructionObject =
+              instructionObject['default'] as Map<String, dynamic>;
+        }
+        break;
+      default:
+        break;
+    }
+
+    final name = retrieveName(step);
+    var instruction = instructionObject['default'] as String;
+    if (step.destinations != null &&
+        step.exits != null &&
+        instructionObject.containsKey('exit_destination')) {
+      instruction = instructionObject['exit_destination'] as String;
+    } else if (step.destinations != null &&
+        instructionObject.containsKey('destination')) {
+      instruction = instructionObject['destination'] as String;
+    } else if (step.exits != null && instructionObject.containsKey('exit')) {
+      instruction = instructionObject['exit'] as String;
+    } else if (name.isNotEmpty && instructionObject.containsKey('name')) {
+      instruction = instructionObject['name'] as String;
+    }
+    var firstDestination = "";
+    try {
+      if (step.destinations != null) {
+        var destinationSplits = step.destinations!.split(':');
+        var destinationRef = destinationSplits.first.split(',').first;
+        if (destinationSplits.length > 1) {
+          var destination = destinationSplits[1].split(',').first;
+          firstDestination = destination;
+          if (destination.isNotEmpty && destinationRef.isNotEmpty) {
+            firstDestination = "$destinationRef: $destination";
+          } else {
+            if (destination.isNotEmpty) {
+              firstDestination = "$destinationRef: $destination";
+            } else if (destinationRef.isNotEmpty) {
+              firstDestination = destinationRef;
+            }
+          }
+        } else {
+          firstDestination = destinationRef;
+        }
+      }
+    } catch (e) {
+      print(e);
+    }
+    String modifierInstruction = "";
+    if (step.maneuver.modifier != null) {
+      modifierInstruction =
+          (instructionsV5["constants"] as Map<String, dynamic>)["modifier"]
+              [step.maneuver.modifier] as String;
+    }
+
+    String nthWaypoint = "";
+    if (option["legIndex"] != null &&
+        option["legIndex"] != -1 &&
+        option["legIndex"] != option["legCount"]) {
+      String key = (option["legIndex"] + 1).toString();
+      nthWaypoint = ordinalize(instructionsV5: instructionsV5, key: key);
+    }
+
+    String exitOrdinalise = "";
+    if (step.maneuver.exit != null) {
+      exitOrdinalise = ordinalize(
+          instructionsV5: instructionsV5, key: step.maneuver.exit.toString());
+    }
+
+    return tokenize(instruction, {
+      "way_name": name,
+      "destination": firstDestination,
+      "exit": step.exits?.split(",").first ?? "",
+      "exit_number": exitOrdinalise,
+      "rotary_name": step.rotaryName ?? "",
+      "lane_instruction": laneInstruction ?? "",
+      "modifier": modifierInstruction,
+      "direction": directionFromDegree(step.maneuver.bearingBefore),
+      "nth": nthWaypoint,
+    });
   }
 }
 
