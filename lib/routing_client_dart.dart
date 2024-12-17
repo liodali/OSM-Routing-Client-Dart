53223c5334c8d5b95baa13b8d1f4a7e1be9745bb
liodali <16631886+liodali@users.noreply.github.com>
2024-12-17 11:27:58 +0100

    forget expot request

--- lib/routing_client_dart.dart
+++ lib/routing_client_dart.dart
@@ -3,6 +3,7 @@ library orsm_dart;
 export 'src/routing_manager.dart' hide RoadManagerUtils;
 export 'src/models/lng_lat.dart' hide PrvExtLngLat;
 export 'src/models/osrm/road.dart' hide PrivateRoad;
+export 'src/models/request_helper.dart' hide BaseRequest;
 export 'src/models/valhalla/valhalla_response.dart';
 export 'src/routes_services/osrm_service.dart';
 export 'src/routes_services/routing_service.dart';
